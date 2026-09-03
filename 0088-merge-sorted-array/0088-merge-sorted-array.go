12b1f9387ad654d78cbda1cdf6021fd32a0041b6
Phạm Bảo Việt <121712725+phambaoviet@users.noreply.github.com>
2026-09-03 17:17:58 +0700

    Time: 0 ms (100%), Space: 4.2 MB (76.36%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.go
@@ -0,0 +1,15 @@
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    p1 := m - 1
+    p2 := n - 1
+    p3 := m + n - 1
+    for p2 >= 0{
+        if p1 >= 0 && nums1[p1] > nums2[p2] {
+            nums1[p3] = nums1[p1]
+            p1--
+        } else {
+            nums1[p3] = nums2[p2]
+            p2--
+        }
+        p3--
+    }
+}
